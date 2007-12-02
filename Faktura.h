4d0f90973407b8fcdd7670172cf053bc35074f89
mind81 <mind81@920ec645-f03f-0410-9fdc-e8aa689e005b>
2007-12-02 13:47:40 +0000

    git-svn-id: https://qfaktury.svn.sourceforge.net/svnroot/qfaktury/trunk@12 920ec645-f03f-0410-9fdc-e8aa689e005b

---
+++ Faktura.h
@@ -0,0 +1,29 @@
+#ifndef Faktura_H
+#define Faktura_H
+#include <QDialog>
+#include "ui_Faktura.h"
+class Faktura: public QDialog, public Ui::Faktura {
+    public:
+    Faktura(QWidget *parent);
+    public slots:
+void init ();
+void readData (QString fraFile, int co);
+void getKontrahent ();
+void addTow ();
+void countRabat ();
+void countSum ();
+void rabatChange ();
+void delTowar ();
+void editTowar ();
+void makeInvoiceHeadar ();
+void makeInvoiceBody ();
+void makeInvoiceGoods ();
+void makeInvoiceSumm ();
+void makeInvoiceSummAll ();
+QString getStawkami();
+void makeInvoice ();
+void saveInvoice ();
+QString numbersCount(int in, int x);
+void backBtnClick();
+};
+#endif
