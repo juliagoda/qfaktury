254b4dc1f99b5f18618776314ce0e651f5e67230
iqmoux <iqmoux@920ec645-f03f-0410-9fdc-e8aa689e005b>
2009-02-18 18:50:47 +0000

    git-svn-id: https://qfaktury.svn.sourceforge.net/svnroot/qfaktury/trunk@85 920ec645-f03f-0410-9fdc-e8aa689e005b

--- MainWindow.h
+++ MainWindow.h
@@ -1,40 +1,49 @@
 #ifndef MainWindow_H
 #define MainWindow_H
 #include <QMainWindow>
+#include <QSettings>
 #include "ui_MainWindow.h"
 class MainWindow: public QMainWindow, public Ui::MainWindow {
-    Q_OBJECT
-void init ();
-bool firstRun ();
-    public:
-    MainWindow(QWidget *parent);
-static void insertRow(QTableWidget *t,int row);
-    public slots:
-void tableClear (QTableWidget * tab);
-void tabChanged (QWidget * aaa);
-void readHist (QString progDir);
-void aboutQt ();
-void oProg ();
-void editFHist ();
-void delFHist ();
-void readKontr (QString progDir);
-void readTw (QString progDir);
-void daneFirmyClick ();
-void settClick ();
-void kontrClick ();
-void kontrDel ();
-void kontrEd ();
-void newFra ();
-void newPForm ();
-void newKor ();
-bool close();
-void pomoc ();
-void towaryDodaj ();
-void towaryUsun ();
-void towaryEdycja ();
-void saveAllSett();
-void nextPage();
-void prevPage();
-    private:
+
+Q_OBJECT
+public:
+	MainWindow(QWidget *parent);
+	static void insertRow(QTableWidget *t, int row);
+public slots:
+	void tableClear(QTableWidget * tab);
+	void tabChanged(QWidget * aaa);
+	void rereadHist();
+	void aboutQt();
+	void oProg();
+	void editFHist();
+	void delFHist();
+	void daneFirmyClick();
+	void settClick();
+	void kontrClick();
+	void kontrDel();
+	void kontrEd();
+	void newFra();
+	void newPForm();
+	void newKor();
+	bool close();
+	void pomoc();
+	void forum();
+	void towaryDodaj();
+	void towaryUsun();
+	void towaryEdycja();
+	void nextPage();
+	void prevPage();
+private:
+	QSettings settings;
+	void saveColumnWidth();
+	bool applyFiltr(QString);
+	void init();
+	bool firstRun();
+	void checkSettings();
+	void saveAllSett();
+	void saveAllSettAsDefault();
+	void readHist(QString progDir);
+	void readKontr(QString progDir);
+	void readTw(QString progDir);
 };
 #endif
