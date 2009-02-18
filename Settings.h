0f50471d86ddd5bfc618fa998c58e71cd30661f3
iqmoux <iqmoux@920ec645-f03f-0410-9fdc-e8aa689e005b>
2009-02-18 18:51:20 +0000

    git-svn-id: https://qfaktury.svn.sourceforge.net/svnroot/qfaktury/trunk@86 920ec645-f03f-0410-9fdc-e8aa689e005b

--- Settings.h
+++ Settings.h
@@ -1,9 +1,25 @@
 #ifndef SETTINGS_H
 #define SETTINGS_H
 #include <QSettings>
+#include <QDate>
+#include <QString>
+#include <Qvariant>
+#include <QLocale>
 #define UTF8(x) QString::fromUtf8(x)
 class Settings: public QSettings {
-    public:
-    Settings():QSettings("elinux","qfaktury") {}
+public:
+	// constr
+	Settings() :
+		QSettings("elinux", "qfaktury") {
+		// setDefaultFormat(QSettings::IniFormat);
+	}
+
+	// get date from settings as QDate
+	QDate getValueAsDate(QString val) {
+		QString date = value(val).toString();
+		QLocale locale;
+		return locale.toDate ( date, QLocale::ShortFormat );
+	}
+
 };
 #endif
