7c566cd15cd47db5d9e404e60f0d966d7cb6681a
iqmoux <iqmoux@920ec645-f03f-0410-9fdc-e8aa689e005b>
2009-02-18 18:55:06 +0000

    git-svn-id: https://qfaktury.svn.sourceforge.net/svnroot/qfaktury/trunk@88 920ec645-f03f-0410-9fdc-e8aa689e005b

--- main.cpp
+++ main.cpp
@@ -4,44 +4,43 @@
 // do splasha
 #include <QLabel>
 #include <QTimer>
+#include <QResource>
 #include <QDir>
+#include <QDesktopWidget>
+#include <QSplashScreen>
+#include <QWaitCondition>
 
 int main (int argc, char **argv)
 {
   QApplication a (argc, argv);
-    
-//  QLabel splash("Hello world!", 0, "", Qt::WStyle_NoBorder|Qt::WStyle_Customize|Qt::WStyle_StaysOnTop | Qt::WShowModal);
-//  splash.resize( 400, 300 );
-//  QDir abs( a.argv()[0] );
-//  QString graphDir;
-//  if ( QString(a.argv()[0]).left(2) == "./" )
-//      graphDir = abs.absPath();
-//      else 
-//      graphDir = "/usr/bin/qfaktury";
-//  //absPath();
-//  graphDir = graphDir.replace( "bin", "share");
-//  qDebug( graphDir );
-//         
-//  splash.setPixmap(QPixmap(graphDir + "/icons/splash.png"));
-//  QRect screen = QApplication::desktop()->screenGeometry();
-//  splash.move(screen.center() - QPoint( splash.width() / 2,
-//                           splash.height() / 2 ) );
-//  splash.show();
-// 
-//  QTimer slip, slip2;
-  MainWindow w(0);
-  //Ui_Form1 wu;
-  //wu.setupUi(&w);
-    //w.move(screen.center() - QPoint( w.width() / 2,  w.height() / 2 ) );
-//  
-//  QObject::connect( &slip, SIGNAL(timeout()), &splash, SLOT(close()) );
-//  slip.start( 4000 );
-//  w.hide();  
-//  
-//  QObject::connect( &slip2, SIGNAL(timeout()), &w, SLOT(show()) );
-//  slip2.start( 3800 );
   
-  w.show();
+  QResource::registerResource("qfaktury.rcc"); 
+  // Q_INIT_RESOURCE(qfaktury);
+
+   QSplashScreen splash(QPixmap(":/res/share/qfaktury/icons/splash.png"));
+   splash.show();
+
+    a.processEvents();
+    
+  QRect screen = QApplication::desktop()->screenGeometry();
+ 
+  MainWindow w(0);
+ 
+   w.move(screen.center() - QPoint( w.width() / 2,  w.height() / 2 ) );
+
+    QTimer *showSplash  = new QTimer();
+    a.connect(showSplash, SIGNAL(timeout()), &w, SLOT(show()));
+    
+
+    QTimer *closeSplash  = new QTimer();
+    a.connect(closeSplash, SIGNAL(timeout()), &splash, SLOT(close()) );
+
+    showSplash->start(5000); 
+    closeSplash->start(4960);
+
+   // splash.finish(&w);
+  
+  // w.show();
   a.connect (&a, SIGNAL (lastWindowClosed ()), &a, SLOT (quit ()));
   return a.exec ();
 }
