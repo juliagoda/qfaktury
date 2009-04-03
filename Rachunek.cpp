86e32822d0ed9205047ac4a35a6d5680f4816ad9
iqmoux <iqmoux@920ec645-f03f-0410-9fdc-e8aa689e005b>
2009-04-03 04:00:23 +0000

    git-svn-id: https://qfaktury.svn.sourceforge.net/svnroot/qfaktury/trunk@186 920ec645-f03f-0410-9fdc-e8aa689e005b

---
+++ Rachunek.cpp
@@ -0,0 +1,22 @@
+/*
+ * Rachunek.cpp
+ *
+ *  Created on: Apr 2, 2009
+ *      Author: moux
+ */
+
+#include "moc_Rachunek.cpp"
+
+// constructor
+Rachunek::Rachunek(QWidget *parent): Faktura(parent) {
+
+}
+
+Rachunek::~Rachunek() {
+
+}
+
+void Rachunek::rachunekInit() {
+
+}
+
---
+++ Rachunek.h
@@ -0,0 +1,34 @@
+/*
+ * Rachunek.h
+ *
+ *  Created on: Apr 2, 2009
+ *      Author: moux
+ */
+
+#ifndef RACHUNEK_H_
+#define RACHUNEK_H_
+
+#include <QDialog>
+#include "ui_Faktura.h"
+#include "Faktura.h"
+
+#include "ConvertAmount.h"
+#include "Settings.h"
+
+
+class Rachunek: public Faktura {
+Q_OBJECT
+public:
+	Rachunek(QWidget *parent);
+	virtual ~Rachunek();
+public slots:
+	void rachunekInit();
+protected:
+
+private:
+
+
+};
+
+
+#endif /* RACHUNEK_H_ */
