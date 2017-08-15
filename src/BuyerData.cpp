04ce06698dde96c801eca3d4f4d3ed02388610f7
Mikołaj Milej <mikolajmm@gmail.com>
2017-08-15 20:23:52 +0200

    Converted BuyerData to final data transfer struct.

--- src/BuyerData.cpp
+++ src/BuyerData.cpp
@@ -1,18 +1,7 @@
 /*
- * KontrData.cpp
- *
  *  Created on: Apr 18, 2009
  *      Author: moux
  */
 
 #include "BuyerData.h"
 
-BuyerData::BuyerData()
-{
-	// TODO Auto-generated constructor stub
-}
-
-BuyerData::~BuyerData()
-{
-	// TODO Auto-generated destructor stub
-}
