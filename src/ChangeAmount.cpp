589bb57df1f3ab69fab242f89593c8370b3196be
Mikołaj Milej <mikolajmm@gmail.com>
2017-08-15 20:24:28 +0200

    Removed uneeded instance() interface from ChangeAmount.

--- src/ChangeAmount.cpp
+++ src/ChangeAmount.cpp
@@ -1,26 +1,7 @@
-
 #include "ChangeAmount.h"
-
-ChangeAmount *ChangeAmount::m_instance = nullptr;
 
 ChangeAmount::ChangeAmount(QWidget *parent)
 	: QDialog(parent)
 {
 	setupUi(this);
-	init();
-}
-
-void ChangeAmount::init()
-{
-	m_instance = this;
-}
-
-ChangeAmount::~ChangeAmount()
-{
-	m_instance = nullptr;
-}
-
-ChangeAmount *ChangeAmount::instance()
-{
-	return m_instance;
 }
